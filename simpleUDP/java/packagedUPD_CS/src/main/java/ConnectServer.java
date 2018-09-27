31037de75db808caae1d9225e8265ae3c88d6376
Martin Nestorov <mnestorov@protonmail.com>
2018-09-27 19:52:43 +0300

    Adder refactored server side code

---
+++ simpleUDP/java/packagedUPD_CS/src/main/java/ConnectServer.java
@@ -0,0 +1,12 @@
+public class ConnectServer {
+
+	public static void main(String args[]) throws Exception {
+
+		UDPServer bookServer = new UDPServer();
+
+		bookServer.listen();
+		
+		bookServer.send();
+	}
+}
+
--- simpleUDP/java/packagedUPD_CS/src/main/java/UDPServer.java
+++ simpleUDP/java/packagedUPD_CS/src/main/java/UDPServer.java
@@ -1,27 +1,52 @@
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
+import java.net.SocketException;
 
-class UDPServer
-{
-    public static void main(String args[]) throws Exception
-    {
-        DatagramSocket serverSocket = new DatagramSocket(9876);
-        byte[] receiveData = new byte[1024];
-        byte[] sendData = new byte[1024];
-        while(true)
-            {
-                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-                serverSocket.receive(receivePacket);
-                String sentence = new String( receivePacket.getData());
-                System.out.println("RECEIVED: " + sentence);
-                InetAddress IPAddress = receivePacket.getAddress();
-                int port = receivePacket.getPort();
-                String capitalizedSentence = sentence.toUpperCase();
-                sendData = capitalizedSentence.getBytes();
-                DatagramPacket sendPacket =
-                    new DatagramPacket(sendData, sendData.length, IPAddress, port);
-                serverSocket.send(sendPacket);
-            }
-    }
+import org.apache.log4j.Logger;
+
+class UDPServer {
+
+	public static final Logger LOGGER = Logger.getLogger(UDPServer.class);
+
+	private String data;
+
+	private byte[] receivedData;
+
+	private DatagramSocket serverSocket;
+
+	private DatagramPacket receivedPacket;
+
+	public UDPServer() throws SocketException {
+
+		receivedData = new byte[1024];
+
+		serverSocket = new DatagramSocket(9876);
+
+		LOGGER.info("Initialized server");
+	}
+
+	public void listen() throws IOException {
+
+		LOGGER.info("Listening on " + 9876);
+
+		while (true) {
+
+			DatagramPacket receivePacket = new DatagramPacket(receivedData, receivedData.length);
+			serverSocket.receive(receivePacket);
+			data = new String(receivePacket.getData());
+			receivedPacket = receivePacket;
+
+			LOGGER.info("Received: " + data);
+		}
+	}
+
+	public void send() throws IOException {
+
+		serverSocket.send(new DatagramPacket(
+				data.toUpperCase().getBytes(),
+				data.length(),
+				receivedPacket.getAddress(),
+				receivedPacket.getPort()));
+	}
 }
