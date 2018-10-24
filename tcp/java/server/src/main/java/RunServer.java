e0a9f6f57ee032f06c3a07d1be8f2abe85c42d25
Martin Nestorov <mnestorov@protonmail.com>
2018-10-24 23:25:14 +0300

    Renamed running server file

--- tcp/java/server/src/main/java/RunServer.java
+++ tcp/java/server/src/main/java/RunServer.java
@@ -1,8 +1,8 @@
 import org.apache.log4j.Logger;
 
-class Server {
+class RunServer {
 
-	private static final Logger LOGGER = Logger.getLogger(Server.class);
+	private static final Logger LOGGER = Logger.getLogger(RunServer.class);
 
 	public static void main(String argv[]) throws Exception {
 
